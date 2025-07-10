97cc941f02323c044b978cc05b35b03a4272944e
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-10 12:45:58 +0600

    new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UtilsService } from './utils/utils.service';
+import { TemplatesService } from './templates/templates.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGO_URL ?? 'mongodb://localhost:27017/nest'),
     UserModule,
+  ],
+  providers: [
+    UtilsService,
+    TemplatesService,
   ],
 })
 export class AppModule {}
