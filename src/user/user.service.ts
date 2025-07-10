fded2acb7ff7c07858df18ae9b0f7208a3dd4f35
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-10 17:51:43 +0600

    create user and fild user methord added

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './user.schema';
+import { CreateUserDto } from './user.dto';
 
 @Injectable()
 export class UserService 
 {
-    constructor() {}
+    constructor(
+        @InjectModel( User.name )
+        private readonly userModel: Model<User>
+    ){}
+
+    async profile( id: string ): Promise<User>
+    {
+        const user = await this.userModel.findById( id )
+        if( !user )
+        {
+            throw new HttpException('User not found', HttpStatus.NOT_FOUND )
+        }
+        return user
+    }
+    
+    async register( data: CreateUserDto ): Promise<User>
+    {
+
+        return this.userModel.create( data )
+    }
+    
 }
