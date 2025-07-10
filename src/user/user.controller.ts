f1e5610ea18b378213cc0544dedcb4178e11cac9
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-10 17:50:32 +0600

    user routes added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './user.dto';
 
 @Controller('user')
 export class UserController 
 {
-    constructor() {}
+    constructor(
+        private readonly userService: UserService,
+    ){}
+
+    @Get('/')
+    profile()
+    {
+        return 'profile';
+    }
+
+    @Post('/')
+    register(
+        @Body() createUserDto: CreateUserDto
+    )
+    {
+        return this.userService.register( createUserDto )
+    }
+
+    @Put('/')
+    update()
+    {
+        return 'update';
+    }
+
+    @Patch('/')
+    change()
+    {
+        return 'change';
+    }
+
+    @Delete('/')
+    delete()
+    {
+        return 'delete';
+    }
+
 }
