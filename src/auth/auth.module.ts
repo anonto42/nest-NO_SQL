33c2cccba6a03cdade5d77b0d4b6a51c0936bdce
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-11 12:28:32 +0600

    added the throtal

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,18 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from 'src/user/user.module';
 import { CommonModule } from 'src/common/common.module';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard } from '@nestjs/throttler';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports:[
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
     UserModule,
     CommonModule
   ],
@@ -13,6 +22,10 @@ import { CommonModule } from 'src/common/common.module';
     AuthController
   ],
   providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
     AuthService
   ]
 })
