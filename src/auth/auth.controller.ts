723fb428da0b7d73967f480af39a44ae4b6d9a8a
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-11 10:00:51 +0600

    register and login

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,23 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/user/user.dto';
+import { LoginUserDto } from './auth.dto';
+
+@Controller('auth')
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService,
+    ) {}
+
+    @Post('register')
+    async register(@Body() registerDto: CreateUserDto) 
+    {
+        return this.authService.register(registerDto);
+    }
+
+    @Post('login')
+    async login(@Body() loginDto: LoginUserDto) 
+    {
+        return this.authService.login(loginDto);
+    }
+}
