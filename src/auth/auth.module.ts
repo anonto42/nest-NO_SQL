1281b998ef3c5e537f13c83f1457eda10c737c40
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-11 10:01:09 +0600

    auth module created

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { CommonModule } from 'src/common/common.module';
+
+@Module({
+  imports:[
+    UserModule,
+    CommonModule
+  ],
+  controllers: [
+    AuthController
+  ],
+  providers: [
+    AuthService
+  ]
+})
+export class AuthModule {}
