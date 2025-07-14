cbbe9d67b6155454de34591035935951e56f5e4d
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-14 14:45:06 +0600

    module added

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,10 +3,12 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './user.schema';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    CommonModule,
   ],
   providers: [UserService],
   controllers: [UserController],
