86866318f945211e68b966b568e48b555d1dbaef
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-14 14:45:34 +0600

    role base authontication added

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Delete, Get, Patch, Post, Put, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Patch, Post, Put, Req, UseGuards, Version } from '@nestjs/common';
 import { UserService } from './user.service';
-import { JwtGuard } from 'src/common/guards/jwt.guard';
+import { JwtAuthGuard } from 'src/common/guards/jwt.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { RoleEnum } from 'src/common/enum/user.enum';
+import { RolesGuard } from 'src/common/guards/roles.guard';
 
 @Controller('user')
 export class UserController 
@@ -9,25 +12,38 @@ export class UserController
         private readonly userService: UserService,
     ){}
 
-    @UseGuards(JwtGuard) // This will use for the authontication
+    @UseGuards(JwtAuthGuard)
+    @Version("1")
     @Get('/')
-    profile()
+    profile( @Req() req: any)
     {
-        return 'profile';
+        return req.user;
     }
 
+    @Version("1")
+    @Post('/')
+    @Roles(RoleEnum.ADMIN, RoleEnum.SUPER_ADMIN, RoleEnum.USER)
+    @UseGuards( JwtAuthGuard, RolesGuard )
+    admin(@Body() body: any)
+    {
+        return body;
+    }
+
+    @Version("1")
     @Put('/')
     update()
     {
         return 'update';
     }
 
+    @Version("1")
     @Patch('/')
     change()
     {
         return 'change';
     }
 
+    @Version("1")
     @Delete('/')
     delete()
     {
