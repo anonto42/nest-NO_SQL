51986ee72843bdbc178f648254c316cb01247e4c
Sohidul Islam Ananto <anontom90@gmail.com>
2025-07-16 12:47:19 +0600

    auth related servicess created

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { ChangePasswordDto, CreateUserDto, ForgotPasswordDto, LoginUserDto, OtpUserDto, RefreshUserDto, VerifyOtpDto } from './auth.dto';
 import { UtilsService } from 'src/common/utils/utils.service';
 import { User, UserModel } from '../user/user.schema';
+import { JwtPayload } from 'jsonwebtoken';
 
 @Injectable()
 export class AuthService {
@@ -17,6 +18,12 @@ export class AuthService {
 
     async register(createUserDto: CreateUserDto): Promise<User>
     {
+        const isUserExist = await this.userModel.findOne( { email: createUserDto.email } )
+
+        if ( isUserExist ) throw new HttpException('User already exists', HttpStatus.BAD_REQUEST )
+
+        if ( createUserDto.password !== createUserDto.confirm_password ) throw new HttpException('Passwords do not match', HttpStatus.BAD_REQUEST )
+
         const user = await this.userModel.create( createUserDto )
 
         if ( !user ) throw new HttpException('User not created', HttpStatus.BAD_REQUEST )
@@ -39,6 +46,7 @@ export class AuthService {
         const refreshToken = this.utilsService.createRefreshToken({id: user._id as string, role: user.role})
 
         user.refreshToken = refreshToken;
+        
         await user.save();
 
         return { user, accessToken, refreshToken }
@@ -46,37 +54,48 @@ export class AuthService {
 
     async refresh(refreshUserDto: RefreshUserDto): Promise<{ accessToken: string }> 
     {
-        const user = await this.userModel.findOne( { refreshToken: refreshUserDto.refreshToken } )
-
-        if ( !user ) throw new HttpException('User not found', HttpStatus.NOT_FOUND )
-
-        const isVarified = await this.utilsService.verifyJwtToken(refreshUserDto.refreshToken)
-
-        if ( !isVarified ) throw new HttpException('Invalid token', HttpStatus.BAD_REQUEST )
-
-        const accessToken = this.utilsService.createAccessToken({id: user._id as string, role: user.role})
-
-        return { accessToken }
+        const user = await this.userModel.findOne({ refreshToken: refreshUserDto.refreshToken });
+    
+        if (!user) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    
+        const isVerified = await this.utilsService.verifyJwtToken(refreshUserDto.refreshToken);
+    
+        if (!isVerified) {
+            throw new HttpException('Invalid token', HttpStatus.BAD_REQUEST);
+        }
+    
+        const decodedToken = await this.utilsService.verifyJwtToken(refreshUserDto.refreshToken); 
+    
+        if (decodedToken.exp * 1000 < Date.now()) {
+            throw new HttpException('Refresh token has expired', HttpStatus.UNAUTHORIZED);
+        }
+    
+        const accessToken = this.utilsService.createAccessToken({ id: user._id as string, role: user.role });
+    
+        return { accessToken };
     }
+    
 
     async otp(otpUserDto: OtpUserDto): Promise<{ message: string, value: null }> 
     {
-        const user = await this.userModel.findOne( { email: otpUserDto.email } )
+        const user = await this.userModel.findOne( { email: otpUserDto.email } );
 
-        if ( !user ) throw new HttpException('User not found', HttpStatus.NOT_FOUND )
+        if ( !user ) throw new HttpException('User not found', HttpStatus.NOT_FOUND );
         
-        const otp = this.utilsService.generateOtp(6)
+        const otp = this.utilsService.generateOtp(6);
 
         user.otp = otp;
+
         user.otpExpiry = new Date(Date.now() + 5 * 60 * 1000);
+
         await user.save();
 
-        this.utilsService.sendMail(user.email, otp,"Here is your OTP for verification:")
+        this.utilsService.sendMail(user.email, otp, "Here is your OTP for verification:");
 
-        return { message: "Otp sent successfully" , value: null }
+        return { message: "Otp sent successfully" , value: null };
     }
 
-    async verifyOtp(verifyOtpDto: VerifyOtpDto): Promise<{ message: string, forget_password_token: string }> 
+    async verifyOtp(verifyOtpDto: VerifyOtpDto): Promise<{ message: string, forget_password_token?: string }> 
     {
         const user = await this.userModel.findOne( { email: verifyOtpDto.email } )
 
@@ -89,17 +108,28 @@ export class AuthService {
         user.otp = undefined;
         
         user.otpExpiry = undefined;
+
+        if ( !user.isVerified ) {
+            
+            user.isVerified = true;
+            
+            await user.save();
+            
+            return { message: "User verified successfully" }
+        };
+
+        const value = this.utilsService.generateOtp(6).toString();
         
-        const value = this.utilsService.generateOtp(5).toString();
-
-        if ( !user.isVerified ) user.resetPasswordToken = value;
-        else if ( user.isVerified ) user.resetPasswordToken = undefined, user.resetPasswordTokenExpiry = new Date( Date.now() + 10 * 60 * 1000 );
-
+        user.resetPasswordToken = value;
+        
+        user.resetPasswordTokenExpiry = new Date(Date.now() + 5 * 60 * 1000);
+        
         await user.save();
-
-        const hash_value = this.utilsService.ganarateHash(value)
-
-        return { message: "Otp verified successfully", forget_password_token:  hash_value }
+        
+        const hash_value = this.utilsService.ganarateHash(value);
+        
+        return { message: "Otp verified successfully", forget_password_token:  hash_value };
+        
     }
 
     async forgotPassword(forgotPasswordDto: ForgotPasswordDto): Promise<{ message: string }> 
@@ -108,7 +138,7 @@ export class AuthService {
 
         if ( !user ) throw new HttpException('User not found', HttpStatus.NOT_FOUND )
 
-        if ( !user.resetPasswordToken ) throw new HttpException('Invalid token', HttpStatus.BAD_REQUEST )
+        if ( !user.resetPasswordToken ) throw new HttpException('Your forgot password token is not ganarated yet', HttpStatus.BAD_REQUEST )
 
         const isTokenValide = this.utilsService.compareHash(user.resetPasswordToken, forgotPasswordDto.token)
 
@@ -116,20 +146,34 @@ export class AuthService {
 
         if ( user.resetPasswordTokenExpiry && user.resetPasswordTokenExpiry < new Date() ) throw new HttpException('Refresh Password Token expired', HttpStatus.BAD_REQUEST )
 
-        if ( forgotPasswordDto.password !== forgotPasswordDto.confirmPassword ) throw new HttpException('Passwords do not match', HttpStatus.BAD_REQUEST )
+        if ( forgotPasswordDto.password !== forgotPasswordDto.confirm_password ) throw new HttpException('Passwords do not match', HttpStatus.BAD_REQUEST )
 
         user.password = forgotPasswordDto.password;
+
         user.resetPasswordToken = undefined;
+
         user.resetPasswordTokenExpiry = undefined;
+
         await user.save();
 
         return { message: "Password changed successfully" }
     }
 
-    async changePassword(changePasswordDto: ChangePasswordDto): Promise<{ message: string }> 
+    async changePassword(user: JwtPayload, changePasswordDto: ChangePasswordDto): Promise<{ message: string }> 
     {
-        
-        console.log(changePasswordDto)
+        if ( changePasswordDto.password !== changePasswordDto.confirm_password ) throw new HttpException('Passwords do not match', HttpStatus.BAD_REQUEST )
+
+        const userFromDB = await this.userModel.findById(user.id)
+
+        if ( !userFromDB ) throw new HttpException('User not found', HttpStatus.NOT_FOUND )
+
+        const isOldPasswordValid = await this.userModel.comparePassword(changePasswordDto.old_password, userFromDB.password)
+
+        if ( !isOldPasswordValid ) throw new HttpException('Invalid old password', HttpStatus.BAD_REQUEST )
+
+        userFromDB.password = changePasswordDto.password;
+
+        await userFromDB.save();
 
         return { message: "Password changed successfully" }
     }
